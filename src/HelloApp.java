1b80428a9860d028f3fcf6c9098afcbf8c32c8b6
guru14114 <gurumoorthi9d@gmail.com>
2026-03-11 08:49:54 +0530

    Initial project structure with src, bin, doc, README and .gitignore

---
+++ src/HelloApp.java
@@ -0,0 +1,14 @@
+/**
+ * Hello App
+ * UC1 - Display Hello World
+ */
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+
+}
