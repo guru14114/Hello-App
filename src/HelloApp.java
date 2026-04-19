778e5a342f3d475c2b3c6d2def1e5d65d2b527ce
Arjun <arjun-email@example.com>
2026-04-19 23:58:04 +0530

    UC5: Display Hello using enhanced for loop

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -2,15 +2,17 @@ public class HelloApp {
     public static void main(String[] args) {
         if (args.length > 0) {
             System.out.print("Hello ");
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i]);
-                if (i != args.length - 1) {
+            int count = 0;
+            for (String name : args) {
+                System.out.print(name);
+                count++;
+                if (count < args.length) {
                     System.out.print(", ");
                 }
             }
             System.out.println();
         } else {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         }
     }
 }
